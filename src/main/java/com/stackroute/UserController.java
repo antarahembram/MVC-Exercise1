fcfc5d23f28865d0e05beb130cc6209d53b1adc0
antara.hembram <antara.hembram@cgi.com>
2019-09-26 09:40:28 +0530

    initial commit

--- src/main/java/com/stackroute/UserController.java
+++ src/main/java/com/stackroute/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
 @RequestMapping("/")
 public class UserController {
@@ -16,8 +15,8 @@ public class UserController {
             public String display(ModelMap model)
             {
                 ApplicationContext context=new AnnotationConfigApplicationContext(UserConfig.class);
-                User user=context.getBean(User.class);
-                model.addAttribute("message", user.getName().toString());
+                User user=context.getBean(User.class);   // creating user object
+                model.addAttribute("message", user.getName().toString());    // setting user name as message and sending to jsp
                 return "index";
             }
 
